af7828ee3c390591ee4ee9bcdd17106ebe5fc9d4
MFC <michael.fernandez.c@tecsup.edu.pe>
2026-08-31 21:25:33 -0500

    Proyecto inicial con menú del carrito

---
+++ Semana02/Lab02CarritoKotlin/carritoConsole/src/main/java/com/fernandez/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,9 @@
+package com.fernandez.lab02carritokotlin
+
+fun main() {
+
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+
+}
