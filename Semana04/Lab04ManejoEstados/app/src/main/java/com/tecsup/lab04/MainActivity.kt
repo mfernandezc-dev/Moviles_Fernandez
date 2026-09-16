4bad53302f53996929cdf989c6ed889867540542
mfernandezc-dev <michael.fernandez.c@tecsup.edu.pe>
2026-09-16 14:36:33 -0500

    Se integro TemperatureDisplay con innerPadding en MainActivity

--- Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/MainActivity.kt
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tecsup.lab04.ui.theme.Lab04ManejoEstadosTheme
+import androidx.compose.foundation.layout.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04ManejoEstadosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        TemperatureDisplay()
+                    }
                 }
             }
         }
