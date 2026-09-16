1c0373732534d2a5184f6c627c9d997a8f5837c9
mfernandezc-dev <michael.fernandez.c@tecsup.edu.pe>
2026-09-16 15:08:41 -0500

    se agrego vista previa PreviewPantallaTareas

--- Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 data class Tarea(
@@ -135,3 +136,11 @@ fun PantallaTareas() {
         }
      }
     }
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPantallaTareas() {
+    MaterialTheme {
+        PantallaTareas()
+    }
+}
