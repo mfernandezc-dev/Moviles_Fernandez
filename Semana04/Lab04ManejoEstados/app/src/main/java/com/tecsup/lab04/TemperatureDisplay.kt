0370a9722cbda271fcf40616a3f214633e3d943a
mfernandezc-dev <michael.fernandez.c@tecsup.edu.pe>
2026-09-16 14:15:20 -0500

    Se agrego contenedor Column y visualizacion del texto de temperatura

--- Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/TemperatureDisplay.kt
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/TemperatureDisplay.kt
@@ -1,11 +1,19 @@
 package com.tecsup.lab04
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TemperatureDisplay() {
@@ -14,5 +22,17 @@ fun TemperatureDisplay() {
         temperatura > 30 -> Color.Red
         temperatura < 10 -> Color.Blue
         else -> Color.Unspecified
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Temperatura: $temperatura °C",
+            style = MaterialTheme.typography.headlineMedium,
+            color = colorTexto
+        )
     }
 }
