8eeb06fe76cc277aee76635deda60d9f5a8fea44
mfernandezc-dev <michael.fernandez.c@tecsup.edu.pe>
2026-09-16 14:49:49 -0500

    se agrego estructura base del componente ItemTarea

--- Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
@@ -1,7 +1,16 @@
 package com.tecsup.lab04
 
+import androidx.compose.runtime.Composable
+
 data class Tarea(
     val id: Int,
     val nombre: String,
     val completada: Boolean = false
 )
+
+@Composable
+fun ItemTarea(
+    tarea: Tarea,
+    onCambiarEstado: (Boolean) -> Unit,
+    onEliminarTarea: () -> Unit
+){}
