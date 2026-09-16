d18ea26c8545d5be5a0f68e550e5cf4908fe0bc3
mfernandezc-dev <michael.fernandez.c@tecsup.edu.pe>
2026-09-16 15:00:48 -0500

    se agrego la tarjeta con la interfaz de ItemTarea

--- Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
@@ -1,6 +1,19 @@
 package com.tecsup.lab04
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 data class Tarea(
     val id: Int,
@@ -13,4 +26,35 @@ fun ItemTarea(
     tarea: Tarea,
     onCambiarEstado: (Boolean) -> Unit,
     onEliminarTarea: () -> Unit
-){}
+){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.weight(1f)
+            ) {
+                Checkbox(
+                    checked = tarea.completada,
+                    onCheckedChange = { onCambiarEstado(it) }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = tarea.nombre,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+            }
+            Button(onClick = onEliminarTarea) {
+                Text("Eliminar")
+            }
+        }
+    }
+}
