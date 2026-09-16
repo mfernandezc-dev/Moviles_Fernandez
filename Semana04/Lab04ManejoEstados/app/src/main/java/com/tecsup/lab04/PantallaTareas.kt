ca6770e78719b995f733954d9fe34d09f06d27c4
mfernandezc-dev <michael.fernandez.c@tecsup.edu.pe>
2026-09-16 15:04:03 -0500

    se agrego estructura inicial del composable PantallaTareas

--- Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
+++ Semana04/Lab04ManejoEstados/app/src/main/java/com/tecsup/lab04/PantallaTareas.kt
@@ -1,8 +1,10 @@
 package com.tecsup.lab04
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -12,6 +14,11 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -58,3 +65,21 @@ fun ItemTarea(
         }
     }
 }
+
+@Composable
+fun PantallaTareas() {
+    var textoTarea by remember { mutableStateOf("") }
+    var contadorId by remember { mutableStateOf(1) }
+    val listaTareas = remember { mutableStateListOf<Tarea>() }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Lista de tareas",
+            style = MaterialTheme.typography.headlineMedium
+        )
+     }
+    }
