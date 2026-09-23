4ac73b25e2c983d60c126f128aa7cb50ac70833f
MFC <michael.fernandez.c@tecsup.edu.pe>
2026-09-23 11:27:37 -0500

    Se implemento la configuracion inicial de navegacion entre pantallas de Clinica Salud

--- Semana05/ClinicaSalud/app/build.gradle.kts
+++ Semana05/ClinicaSalud/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.fernandez.clinicasalud"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -38,6 +38,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
--- Semana05/ClinicaSalud/app/src/main/java/com/fernandez/clinicasalud/navegacion/AppNavegacion.kt
+++ Semana05/ClinicaSalud/app/src/main/java/com/fernandez/clinicasalud/navegacion/AppNavegacion.kt
@@ -1,2 +1,44 @@
 package com.fernandez.clinicasalud.navegacion
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.fernandez.clinicasalud.screens.InicioScreen
+import com.fernandez.clinicasalud.screens.PerfilMedicosScreen
+
+@Composable
+fun AppNavegacion() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "inicio"
+    ) {
+
+        composable("inicio") {
+
+            InicioScreen(
+                navController = navController
+            )
+
+        }
+
+
+        composable(
+            route = "perfil/{nombre}"
+        ) { backStackEntry ->
+
+            val nombre =
+                backStackEntry.arguments?.getString("nombre") ?: ""
+
+            PerfilMedicosScreen(
+                nombre = nombre
+            )
+
+        }
+
+    }
+
+}
