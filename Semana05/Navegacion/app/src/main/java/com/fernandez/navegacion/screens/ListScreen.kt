947cb7bde25e5b20c7e44d33ec80058e124d236a
MFC <michael.fernandez.c@tecsup.edu.pe>
2026-09-22 22:58:27 -0500

    Se implemento ListScreen con lista y navegación

--- Semana05/Navegacion/app/src/main/java/com/fernandez/navegacion/screens/ListScreen.kt
+++ Semana05/Navegacion/app/src/main/java/com/fernandez/navegacion/screens/ListScreen.kt
@@ -1,2 +1,68 @@
 package com.fernandez.navegacion.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.fernandez.navegacion.navigation.Screen
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListScreen(navController: NavController) {
+
+    val items = (1..8).map {
+        "Elemento número $it"
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Lista")
+                },
+
+                navigationIcon = {
+
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    ) {
+
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Volver"
+                        )
+                    }
+                }
+            )
+        }
+
+    ) { padding ->
+        LazyColumn(
+            contentPadding = padding
+        ) {
+            items(items.size) { index ->
+                ListItem(
+                    headlineContent = {
+                        Text(items[index])
+                    },
+                    supportingContent = {
+                        Text("Toca para ver el detalle")
+                    },
+                    modifier = Modifier.clickable {
+                        navController.navigate(
+                            Screen.Detail.createRoute(index + 1)
+                        )
+                    }
+                )
+                HorizontalDivider()
+            }
+        }
+    }
+}
